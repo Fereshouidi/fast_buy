a174b9c4e610d126b02bc874757a1ae2410bed46
fares <fereshouidi298@gmail.com>
2024-11-28 23:11:18 +0100

    edit

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,45 +1,42 @@
-
 "use client";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import HeaderForComputer from "../../components/header/headerForComputer/header";
 import HeaderForPhone from "../../components/header/headerForPhones/header";
 
-
 const Home = () => {
+  const router = useRouter();
 
-    const router = useRouter()
+  const [screenWidth, setScreenWidth] = useState(0); 
 
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  // handle screen width
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenWidth(window.innerWidth);
 
-    // handle screen width
-    
-    useEffect(() => {
+      const handleResize = () => {
+        setScreenWidth(window.innerWidth);
+      };
 
-        const handleResize = () => {
-            setScreenWidth(window.innerWidth);
-        }
+      window.addEventListener("resize", handleResize);
 
-        
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-          };
-
-    }, [])
-    
-
-    const goToAbout = () => {
-        router.push('/pages/about');    
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
     }
+  }, []);
 
-    return(
-        <>
-            {screenWidth > 700 ? <HeaderForComputer/> : <HeaderForPhone/>} 
-            <div>home page</div>
-            <button onClick={goToAbout}></button>
-        </>
-    )
-}
-export default  Home;
+  const goToAbout = () => {
+    router.push("/pages/about");
+  };
+
+  return (
+    <>
+      {screenWidth > 700 ? <HeaderForComputer /> : <HeaderForPhone />}
+      <div>home page</div>
+      <button onClick={goToAbout}>Go to About</button>
+    </>
+  );
+};
+
+export default Home;
