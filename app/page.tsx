dd164c89b07578ee7bcdb5d3122ad448413b6fca
fares <fereshouidi298@gmail.com>
2024-11-28 22:37:37 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import styles from "./page.module.css";
-import ShoppingCartIcon from './svg/icons/shoppingCart';
 import { useRouter } from "next/navigation";
-import Home from "./pages/home/page";
-import About from "./pages/about/page";
 
 export default function App() {
 
