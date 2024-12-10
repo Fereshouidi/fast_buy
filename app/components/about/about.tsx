23276cda8e7909dd4103a67417f4e4c7f083e0bc
fares <fereshouidi298@gmail.com>
2024-12-11 00:09:36 +0100

    edit

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -22,7 +22,6 @@ const About = () => {
         catgorie: categorieParams;
         products: productParams[];
     }[];
-    
 
     type bigestDiscountParams = {
         catgorie: categorieParams;
@@ -52,7 +51,6 @@ const About = () => {
         arabic: string
     }
 
-    
     type discountParams = {
         createdAt: Date;
         discountSticker: string;
@@ -96,133 +94,131 @@ const About = () => {
         fetchData();
     }, [])
 
-
-    return (
          //<section id="about-section" dangerouslySetInnerHTML={{ __html: activeLanguage.aboutP }} />
 
-        conpanyInformations && languageContext.activeLanguage === "english" ?
-        <section id="about-section">
-            <h1>{' ' + conpanyInformations.name.english + ' '}: The Leading E-Commerce Platform for Your Customers</h1>
-            <p>
-                Welcome to <strong>{' ' + conpanyInformations.name.english + ' '}</strong>, your trusted partner for an exceptional online shopping experience tailored to your customers' needs. We offer:
-            </p>
-
-            <div className="products">
-                <h2>A Wide Range of Products:</h2>
-                <ul>
+         return (
+            conpanyInformations && languageContext.activeLanguage === "english" ?
+              <section id="about-section">
+                <h1>{`${conpanyInformations.name.english}: The Leading E-Commerce Platform for Your Customers`}</h1>
+                <p>
+                  Welcome to <strong>{`${conpanyInformations.name.english}`}</strong>, your trusted partner for an exceptional online shopping experience tailored to your customers' needs. We offer:
+                </p>
+          
+                <div className="products">
+                  <h2>A Wide Range of Products:</h2>
+                  <ul>
                     {bestCategories?.map((categorie, index) => (
-                        <li key={index}>
-                            <strong>{' ' + categorie.catgorie.name.english + ' '}:</strong> such as 
-                            {categorie.products.slice(0, 3).map((product, index) => (
-                                <span key={index}>{' ' + product.name.english + ' '}</span>
-                            ))}
-                        .</li>
-                    ))}
-                </ul>
-            </div>
-
-            <div className="offers">
-                <h2>Exclusive Offers and Discounts:</h2>
-                <ul>
-                    <li>
-                        Enjoy discounts up to <strong>{' ' + conpanyInformations.biggestDiscount}%{' '}</strong> 
-                        on {bigestDiscount && bigestDiscount.products.length >= 0 ? ' ' + bigestDiscount.products[0].name.english + ' ' : ''}
-                    </li>
-                    <li>
-                        Exclusive offers such as <strong>{' ' + conpanyInformations.offersDetails + ' '}</strong>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="services">
-                <h2>Exceptional Services:</h2>
-                <ul>
-                    {conpanyInformations.servises && conpanyInformations.servises.map((servise, index) => {
-                        return <li key={index}>{' ' + servise + ' .'}</li>
-                    })}
-                </ul>
-            </div>
-
-            <div className="guarantee">
-                <h2>Quality Guarantee:</h2>
-                <ul>
-                    <li>
-                        All our products are <strong>{' ' + conpanyInformations.originalProductsPercentage}%{' '}</strong> 
-                        authentic, with flexible return policies.
-                    </li>
-                </ul>
-            </div>
-
-            <p>
-                Discover now the world of <strong>{' ' + conpanyInformations.name.english + ' '}</strong> and be part of a shopping experience 
-                that makes your life easier and more enjoyable. Don't miss the opportunity to join thousands of satisfied customers who trust us!
-            </p>
-        </section>
-
-
-        : conpanyInformations && languageContext.activeLanguage === "arabic" ?
-        
-        <section id="about-section">
-            <h1>{' ' + conpanyInformations.name.arabic + ' '}: المنصة الرائدة للتجارة الإلكترونية لعملائك</h1>
-            <p>
-                مرحبًا بك في <strong>{' ' + conpanyInformations.name.arabic + ' '}</strong>، شريكك الموثوق لتجربة تسوق إلكتروني استثنائية تناسب احتياجات عملائك. نحن نقدم:
-            </p>
-
-            <div className="products">
-                <h2>مجموعة واسعة من المنتجات:</h2>
-                <ul>
-                    {bestCategories?.map((categorie, index) => (
-                        <li key={index}>
-                            <strong>{' ' + categorie.catgorie.name.arabic + ' '}:</strong> مثل 
-                            {categorie.products.slice(0, 3).map((product, index) => (
-                                <span key={index}>{' ' + product.name.arabic + ' '}</span>
-                            ))}
+                      <li key={index}>
+                        <strong>{`${categorie.catgorie.name.english}:`}</strong> such as
+                        {categorie.products.slice(0, 3).map((product, index) => (
+                          <span key={index}>{`${product.name.english}`}</span>
+                        ))}
                         .
-                        </li>
+                      </li>
                     ))}
-                </ul>
-            </div>
-
-            <div className="offers">
-                <h2>عروض وخصومات حصرية:</h2>
-                <ul>
+                  </ul>
+                </div>
+          
+                <div className="offers">
+                  <h2>Exclusive Offers and Discounts:</h2>
+                  <ul>
                     <li>
-                        استمتع بخصومات تصل إلى <strong>{' ' + conpanyInformations.biggestDiscount}%{' '}</strong> 
-                        على {bigestDiscount && bigestDiscount.products.length >= 0 ? ' ' + bigestDiscount.products[0].name.arabic + ' ' : ''}
+                      Enjoy discounts up to <strong>{`${conpanyInformations.biggestDiscount}%`}</strong>
+                      on {bigestDiscount && bigestDiscount.products.length >= 0 ? ` ${bigestDiscount.products[0].name.english} ` : ''}
                     </li>
                     <li>
-                        عروض حصرية مثل <strong>{' ' + conpanyInformations.offersDetails + ' '}</strong>
+                      Exclusive offers such as <strong>{`${conpanyInformations.offersDetails}`}</strong>
                     </li>
-                </ul>
-            </div>
-
-            <div className="services">
-                <h2>خدمات استثنائية:</h2>
-                <ul>
-                    {conpanyInformations.servises && conpanyInformations.servises.map((servise, index) => {
-                        return <li key={index}>{' ' + servise + ' .'}</li>
-                    })}
-                </ul>
-            </div>
-
-            <div className="guarantee">
-                <h2>ضمان الجودة:</h2>
-                <ul>
+                  </ul>
+                </div>
+          
+                <div className="services">
+                  <h2>Exceptional Services:</h2>
+                  <ul>
+                    {conpanyInformations.servises && conpanyInformations.servises.map((servise, index) => (
+                      <li key={index}>{`${servise}.`}</li>
+                    ))}
+                  </ul>
+                </div>
+          
+                <div className="guarantee">
+                  <h2>Quality Guarantee:</h2>
+                  <ul>
                     <li>
-                        جميع منتجاتنا أصلية بنسبة <strong>{' ' + conpanyInformations.originalProductsPercentage}%{' '}</strong> 
-                        مع سياسات إرجاع مرنة.
+                      All our products are <strong>{`${conpanyInformations.originalProductsPercentage}%`}</strong>
+                      authentic, with flexible return policies.
                     </li>
-                </ul>
-            </div>
-
-            <p>
-                اكتشف الآن عالم <strong>{' ' + conpanyInformations.name.arabic + ' '}</strong> وكن جزءًا من تجربة تسوق 
-                تجعل حياتك أسهل وأكثر متعة. لا تفوت الفرصة للانضمام إلى آلاف العملاء الراضين الذين يثقون بنا!
-            </p>
-        </section>
-
-
-        : ''
-    )
+                  </ul>
+                </div>
+          
+                <p>
+                  Discover now the world of <strong>{`${conpanyInformations.name.english}`}</strong> and be part of a shopping experience
+                  that makes your life easier and more enjoyable. Don't miss the opportunity to join thousands of satisfied customers who trust us!
+                </p>
+              </section>
+          
+              : conpanyInformations && languageContext.activeLanguage === "arabic" ?
+              <section id="about-section">
+                <h1>{`${conpanyInformations.name.arabic}: المنصة الرائدة للتجارة الإلكترونية لعملائك`}</h1>
+                <p>
+                  مرحبًا بك في <strong>{`${conpanyInformations.name.arabic}`}</strong>، شريكك الموثوق لتجربة تسوق إلكتروني استثنائية تناسب احتياجات عملائك. نحن نقدم:
+                </p>
+          
+                <div className="products">
+                  <h2>مجموعة واسعة من المنتجات:</h2>
+                  <ul>
+                    {bestCategories?.map((categorie, index) => (
+                      <li key={index}>
+                        <strong>{`${categorie.catgorie.name.arabic}:`}</strong> مثل
+                        {categorie.products.slice(0, 3).map((product, index) => (
+                          <span key={index}>{`${product.name.arabic}`}</span>
+                        ))}
+                        .
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+          
+                <div className="offers">
+                  <h2>عروض وخصومات حصرية:</h2>
+                  <ul>
+                    <li>
+                      استمتع بخصومات تصل إلى <strong>{`${conpanyInformations.biggestDiscount}%`}</strong>
+                      على {bigestDiscount && bigestDiscount.products.length >= 0 ? ` ${bigestDiscount.products[0].name.arabic} ` : ''}
+                    </li>
+                    <li>
+                      عروض حصرية مثل <strong>{`${conpanyInformations.offersDetails}`}</strong>
+                    </li>
+                  </ul>
+                </div>
+          
+                <div className="services">
+                  <h2>خدمات استثنائية:</h2>
+                  <ul>
+                    {conpanyInformations.servises && conpanyInformations.servises.map((servise, index) => (
+                      <li key={index}>{`${servise}.`}</li>
+                    ))}
+                  </ul>
+                </div>
+          
+                <div className="guarantee">
+                  <h2>ضمان الجودة:</h2>
+                  <ul>
+                    <li>
+                      جميع منتجاتنا أصلية بنسبة <strong>{`${conpanyInformations.originalProductsPercentage}%`}</strong>
+                      مع سياسات إرجاع مرنة.
+                    </li>
+                  </ul>
+                </div>
+          
+                <p>
+                  اكتشف الآن عالم <strong>{`${conpanyInformations.name.arabic}`}</strong> وكن جزءًا من تجربة تسوق
+                  تجعل حياتك أسهل وأكثر متعة. لا تفوت الفرصة للانضمام إلى آلاف العملاء الراضين الذين يثقون بنا!
+                </p>
+              </section>
+          
+              : ''
+          )
+          
 }
 export default About;
