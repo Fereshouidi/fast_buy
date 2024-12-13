7692e6d4556cab6a937d40fad561e9d3e7823c68
fares <fereshouidi298@gmail.com>
2024-12-13 11:52:56 +0100

    edit

--- app/pages/productDetails/[productId]/page.tsx
+++ app/pages/productDetails/[productId]/page.tsx
@@ -1,12 +1,27 @@
 'use client';
 
+import { useEffect, useState } from "react";
+
+interface propsParams {
+    params: Promise<Params>;
+    searchParams: string
+}
 interface Params {
     productId: string;
 }
-const ProductDetails = ({params}: {params: Params}) => {
 
-    const {productId} = params
-    console.log(typeof params);
+const ProductDetails = (props: propsParams) => {
+
+    const [productId, setProductId] = useState<string>();
+
+    useEffect(() => {
+        const getProductd = async() => {
+            console.log( props.params);
+            const productId = await props.params;
+            setProductId(productId.productId)
+        }
+        getProductd()
+    }, [])
     
     return (
         <div>product details {productId} </div>
