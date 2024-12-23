ef3a75bb2893d4a5de70d83cbf3544f26e4dd8a6
fares <fereshouidi298@gmail.com>
2024-12-23 20:46:15 +0100

    edit

--- app/pages/register/component/header/ForComputer/components/logo/logo.tsx
+++ app/pages/register/component/header/ForComputer/components/logo/logo.tsx
@@ -20,7 +20,7 @@ const Logo = () => {
                 src={companyInformations.logo}
                 alt="Logo"   
             />
-            <h1>{companyInformations && companyInformations.name.english}</h1>
+            <h1>{companyInformations && companyInformations.name?.english}</h1>
         </div>
     )
 }
